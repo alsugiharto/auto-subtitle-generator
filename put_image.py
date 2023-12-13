a64168f2dd40a1b8cec927858db2891d1c6a26ca
al <a.k.sugiharto@gmail.com>
2023-12-13 17:45:46 +0100

    add comment for putting image

--- put_image.py
+++ put_image.py
@@ -1,20 +1,7 @@
 import numpy as np
-from moviepy.editor import *
+from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 import yaml
 import math
-
-# load yaml
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-print('CONFIG')
-print(config)
-
-video_path = f"{config['FOLDER_PATH']}source.mp4"
-image_path = f"{config['FOLDER_PATH']}download.jpeg"
-output_video_path = f"{config['FOLDER_PATH']}result_put_image.mp4"
-
-video_clip = VideoFileClip(video_path)
-image_clip = ImageClip(image_path).set_duration(video_clip.duration)
 
 def motion_crossing_leftup_rightdown(t):
     size_x = video_clip.size[0]
@@ -28,6 +15,25 @@ def motion_circleing_in_middle(t):
     radius = video_clip.size[1] * 0.2
     return center_x + radius * np.cos((t/video_clip.duration) * 2 * math.pi), center_y + radius * np.sin((t/video_clip.duration) * 2 * math.pi)
 
+
+# load yaml
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+print('CONFIG')
+print(config)
+
+# constants
+video_path = f"{config['FOLDER_PATH']}source.mp4"
+image_path = f"{config['FOLDER_PATH']}download.jpeg"
+output_video_path = f"{config['FOLDER_PATH']}result_put_image.mp4"
+
+# get video
+video_clip = VideoFileClip(video_path)
+# get the image
+image_clip = ImageClip(image_path).set_duration(video_clip.duration)
+# set position of the image
 image_clip = image_clip.set_position(motion_circleing_in_middle)
+# combine the image and video
 final_clip = CompositeVideoClip([video_clip, image_clip])
+# generate video
 final_clip.write_videofile(output_video_path, fps=video_clip.fps)
